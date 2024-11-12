e17f5b1096beb25e08f58095c6562ab6025ac6f9
asjedh-dilimatrix <Mohamed.Asjedh@dilimatrix.com>
2024-11-12 08:19:36 +0530

    middleware added to the project

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,52 @@
 "use client";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function ProfilePage() {
-  return <div>Profile</div>;
+  const router = useRouter();
+  const [data, setData] = useState("nothing");
+
+  const logout = async () => {
+    try {
+      const response = await axios.get("/api/users/logout");
+      console.log("Logout Succeesfull", response.data);
+      router.push("/login");
+    } catch (error: any) {
+      console.log("User didn't logout", error.message);
+    }
+  };
+
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
+
+  return (
+    <div>
+      <h1>Profile</h1>
+      <p>Welcome to your profile page</p>
+      <h2>
+        {data === "nothing" ? (
+          "Nothing"
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
+      <button
+        onClick={logout}
+        className="bg-blue-500 text-white font-semibold p-2 rounded w-80 hover:bg-blue-600"
+      >
+        Logout
+      </button>
+      <button
+        onClick={getUserDetails}
+        className="bg-blue-500 text-white font-semibold p-2 rounded w-80 hover:bg-blue-600"
+      >
+        Get User Details
+      </button>
+    </div>
+  );
 }
